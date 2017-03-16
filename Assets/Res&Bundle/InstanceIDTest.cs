29928167772950d6d6bb4dd5b13e0d7cbf5daf9b
zhangyi <nickzy@163.com>
2017-03-16 09:46:37 +0800

    增加bundle 测试相关的目录

---
+++ Assets/Res&Bundle/InstanceIDTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstanceIDTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject prefab = Resources.Load<GameObject>("cube/Cube");
+		Debug.Log("prefab " + prefab.GetInstanceID());
+
+		GameObject instance = GameObject.Instantiate(prefab);
+		Debug.Log("instance " + instance.GetInstanceID());
+
+		GameObject prefab2 = Resources.Load<GameObject>("cube/Cube");
+		Debug.Log("prefab2 " + prefab2.GetInstanceID());
+
+		GameObject instance2 = GameObject.Instantiate(prefab2);
+		Debug.Log("instance2 " + instance2.GetInstanceID());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
